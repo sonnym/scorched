81bb44791af9f8d412dd6e94baad74d879586c3a
sonnym <michaud.sonny@gmail.com>
2014-12-29 00:01:34 -0500

    use keysDown for input signal

--- Scorched/Input.elm
+++ Scorched/Input.elm
@@ -1,11 +1,9 @@
 module Scorched.Input where
 
-import Signal (Signal, merge, map, sampleOn, constant)
-import Time (every, millisecond)
+import Signal (Signal, map)
+import Keyboard (KeyCode, keysDown)
 
-import Keyboard (KeyCode, lastPressed)
-
-type Input = Nothing | Just KeyCode
+type alias Input = List KeyCode
 
 keypress : Signal Input
-keypress = merge (map Just lastPressed) (sampleOn (every millisecond) (constant Nothing))
+keypress = keysDown
