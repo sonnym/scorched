cd1a8380636df4ddd252e855aa83d6d431f0c729
sonnym <michaud.sonny@gmail.com>
2014-12-30 19:16:48 -0500

    scaffolding for key label hooks

--- Scorched/Model.elm
+++ Scorched/Model.elm
@@ -1,7 +1,15 @@
 module Scorched.Model where
 
-import Signal (Signal, Message, foldp, subscribe)
+import List
+import List ((::), filter, length, head)
 
+import Signal
+import Signal (Signal, Message, foldp, subscribe, merge)
+
+import Char (toCode)
+import Keyboard (KeyCode)
+
+import Scorched.Input (Input, keypress)
 import Scorched.Action (Action(NoOp, PlayerCount, Start), updates)
 
 import Scorched.Model.World (Dimension)
@@ -15,6 +23,7 @@ type alias Messenger = Int -> Message
 
 type alias Model = {
   view: View,
+  hooks: List (Char, Action),
   dimensions: Dimension,
   playerCount: Int,
   roundCount: Int,
@@ -22,10 +31,20 @@ type alias Model = {
 }
 
 state : Signal Model
-state = foldp step default (subscribe updates)
+state = foldp step default (Signal.map2 (,) (subscribe updates) keypress)
 
-step : Action -> Model -> Model
-step action model =
+step : (Action, Input) -> Model -> Model
+step (action, input) ({hooks} as model) =
+  applicator (action :: (List.map (lookup hooks) input)) model
+
+applicator : List Action -> Model -> Model
+applicator actions model =
+  case actions of
+    action::rest -> applicator rest (apply action model)
+    [] -> model
+
+apply : Action -> Model -> Model
+apply action model =
   case action of
     NoOp -> model
     PlayerCount value -> { model | playerCount <- value }
@@ -34,9 +53,18 @@ step action model =
       { model | view <- Game
               , game <- GameState.default }
 
+lookup : List (Char, Action) -> KeyCode -> Action
+lookup hooks keyCode =
+  let
+    match = filter (\pair -> (toCode (fst pair)) == keyCode) hooks
+  in
+    if | length match > 0 -> snd (head match)
+       | otherwise -> NoOp
+
 default : Model
 default = {
   view = Menu,
+  hooks = [],
   dimensions = {width=1024, height=768},
   playerCount = 2,
   roundCount = 10,
