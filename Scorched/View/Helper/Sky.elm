aba7adc379d07a728fedc1125fcfd63869ad689e
sonnym <michaud.sonny@gmail.com>
2015-01-02 00:35:58 -0500

    fix sky overflowing container

--- Scorched/View/Helper/Sky.elm
+++ Scorched/View/Helper/Sky.elm
@@ -2,10 +2,13 @@ module Scorched.View.Helper.Sky where
 
 import List (map, length)
 
-import Graphics.Collage (Form)
+import Graphics.Collage (Form, groupTransform)
 import Graphics.Collage as Collage
 
-import Graphics.Element (flow, down, empty, size, color)
+import Graphics.Element as Element
+import Graphics.Element (flow, down, empty, size, color, heightOf)
+
+import Transform2D (scaleY)
 
 import Scorched.Model.Geometry (Dimension)
 import Scorched.Model.Sky (Sky)
@@ -14,6 +17,8 @@ toForm : Sky -> Dimension -> Form
 toForm sky {width,height} =
   let
     bandHeight = ceiling ((toFloat height) / (toFloat (length sky)))
+    elem = flow down (map (\band -> color band (size width bandHeight empty)) sky)
   in
-    Collage.toForm (flow down
-      (map (\band -> color band (size width bandHeight empty)) sky))
+    groupTransform
+      (scaleY ((toFloat height) / (toFloat (heightOf elem))))
+      [ Collage.toForm elem ]
