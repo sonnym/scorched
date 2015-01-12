02060b0ad2493ad0f24963e8b53946374a106168
sonnym <michaud.sonny@gmail.com>
2015-01-11 21:39:35 -0500

    remove use of elements when rendering sunset

--- Scorched/View/Helper/Sky/Sunset.elm
+++ Scorched/View/Helper/Sky/Sunset.elm
@@ -1,14 +1,11 @@
 module Scorched.View.Helper.Sky.Sunset where
 
 import Color (Color)
-import List (map, length)
+import List (indexedMap, length)
 
-import Graphics.Collage as Collage
-import Graphics.Collage (Form, groupTransform)
+import Graphics.Collage (Form, groupTransform, moveY, filled, rect)
 
-import Transform2D (scaleY)
-
-import Graphics.Element (flow, down, empty, size, color, heightOf)
+import Transform2D (translation)
 
 import Scorched.Model.Geometry (Dimension)
 import Scorched.Model.Sky.Sunset (Sunset)
@@ -16,9 +13,12 @@ import Scorched.Model.Sky.Sunset (Sunset)
 toForm : Sunset -> Dimension -> Form
 toForm sky {width,height} =
   let
-    bandHeight = ceiling ((toFloat height) / (toFloat (length sky)))
-    elem = flow down (map (\band -> color band (size width bandHeight empty)) sky)
+    bandHeight = (toFloat height) / (toFloat (length sky))
+    translationY = ((toFloat height) - bandHeight) / 2.0
   in
     groupTransform
-      (scaleY ((toFloat height) / (toFloat (heightOf elem))))
-      [ Collage.toForm elem ]
+      (translation 0.0 translationY)
+      (indexedMap (\i color ->
+        rect (toFloat width) bandHeight
+          |> filled color
+          |> moveY -(bandHeight * (toFloat i))) sky)
