0191abf3e0abfce45029c87bda44f0285f0773d2
Sonny Michaud <michaud.sonny@gmail.com>
2018-10-20 14:24:24 -0500

    basic element display

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,32 @@
-import Graphics.Element exposing (Element)
+import Browser
+import Html exposing (Html, h1, text)
+-- import Graphics.Element exposing (Element)
 
-import Scorched.State as State
-import Scorched.View as View
+-- import Scorched.State as State
+-- import Scorched.View as View
 
-main : Signal Element
-main = Signal.map View.scene State.state
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+init : () -> (Int, Cmd msg)
+init flags =
+  ( 1
+  , Cmd.none
+  )
+
+update : msg -> Int -> (Int, Cmd msg)
+update msg model =
+  ( 1
+  , Cmd.none
+  )
+
+subscriptions : Int -> Sub msg
+subscriptions model = Sub.none
+
+view : Int -> Html msg
+view model = h1 [] [ text (String.fromInt model) ]
