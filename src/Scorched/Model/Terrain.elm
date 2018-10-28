515049950ac2a3edd7488e845d22771dbc0548a8
Sonny Michaud <michaud.sonny@gmail.com>
2018-10-28 06:33:44 -0500

    initial noise generation

--- src/Scorched/Model/Terrain.elm
+++ src/Scorched/Model/Terrain.elm
@@ -1,6 +1,7 @@
 module Scorched.Model.Terrain exposing (empty, random)
 
 import Random
+import Scorched.Model.Noise as Noise
 
 import Scorched.Model.Types exposing (Action(..), Terrain)
 import Scorched.Model.Geometry exposing (Dimension)
@@ -11,7 +12,16 @@ random dimension =
 
 generator : Dimension -> Random.Generator Terrain
 generator {width, height} =
-  Random.list width (Random.int 0 (height // 4))
+  combine
+    (List.map
+      (\x -> Random.map (\n -> round (n * (toFloat (height // 2)))) (Noise.generator x))
+      (List.map (\n -> (0.002 * toFloat n)) (List.range 1 width)))
 
 empty : Terrain
 empty = []
+
+combine : List (Random.Generator a) -> Random.Generator (List a)
+combine generators =
+  case generators of
+    [] -> Random.constant []
+    g :: gs -> Random.map2 (::) g (combine gs)
