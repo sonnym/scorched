8344411381c9949f35724f98d10e5b32b42a5cf0
Sonny Michaud <michaud.sonny@gmail.com>
2018-10-28 20:05:11 -0500

    minor improvements to terrain rendering

--- src/Scorched/View/Terrain.elm
+++ src/Scorched/View/Terrain.elm
@@ -6,20 +6,23 @@ import Svg.Attributes as Attr
 import Scorched.Model.Types exposing (Terrain)
 import Scorched.Model.Geometry exposing (Dimension)
 
-build : Terrain -> Dimension -> List (Svg msg)
+build : Terrain -> Dimension -> Svg msg
 build terrain {height} =
-  List.map2
-    (buildLine height)
-    (List.range 0 (List.length terrain - 1))
-    terrain
+  Svg.g
+    [ ]
+    (List.map2
+      (buildLine height)
+      (List.range 0 (List.length terrain - 1))
+      terrain)
 
 buildLine : Int -> Int -> Int -> Svg msg
-buildLine bottom yOffset height =
+buildLine bottom xOffset height =
   Svg.line
-    [ Attr.x1 (String.fromInt yOffset)
-    , Attr.x2 (String.fromInt yOffset)
+    [ Attr.x1 (String.fromInt xOffset)
+    , Attr.x2 (String.fromInt xOffset)
     , Attr.y1 (String.fromInt bottom)
     , Attr.y2 (String.fromInt (bottom - height))
+    , Attr.strokeWidth "1"
     , Attr.stroke "rgb(142, 216, 112)"
     ]
     []
