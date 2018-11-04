544b6d6e03f609ddc33b4b7d2656b7422edadb43
Sonny Michaud <michaud.sonny@gmail.com>
2018-11-03 19:24:32 -0500

    render all controls

--- src/Scorched/Model/MainMenu.elm
+++ src/Scorched/Model/MainMenu.elm
@@ -20,18 +20,16 @@ defaultControls =
     (List.map
       (\control -> (control.label, control))
         [ Control "Start" 'S' {x=7, y=12} (Button (ButtonSpec {width=82, height=19} False NoOp))
+        , Control "Sound…" 'o' {x=7, y=102} (Button (ButtonSpec {width=56, height=19} False NoOp))
+        , Control "Hardware…" 'H' {x=7, y=132} (Button (ButtonSpec {width=78, height=19} False NoOp))
+        , Control "Economics…" 'E' {x=7, y=162} (Button (ButtonSpec {width=84, height=19} False NoOp))
+        , Control "Physics…" 'y' {x=7, y=192} (Button (ButtonSpec {width=70, height=19} False NoOp))
+        , Control "Landscape…" 'L' {x=7, y=222} (Button (ButtonSpec {width=82, height=19} False (UpdateView (SubMenu Landscape))))
+        , Control "Play Options…" 't' {x=7, y=252} (Button (ButtonSpec {width=98, height=19} False NoOp))
+        , Control "Weapons…" 'W' {x=7, y=282} (Button (ButtonSpec {width=68, height=19} False NoOp))
 
-        {--
-        , Control Button "Sound…" 'o' {width=56, height=19} {x=7, y=102} False NoOp
-        , Control Button "Hardware…" 'H' {width=78, height=19} {x=7, y=132} False NoOp
-        , Control Button "Economics…" 'E' {width=84, height=19} {x=7, y=162} False NoOp
-        , Control Button "Physics…" 'y' {width=70, height=19} {x=7, y=192} False NoOp
-        , Control Button "Landscape…" 'L' {width=82, height=19} {x=7, y=222} False (UpdateView (SubMenu Landscape))
-        , Control Button "Play Options…" 't' {width=98, height=19} {x=7, y=252} False NoOp
-        , Control Button "Weapons…" 'W' {width=68, height=19} {x=7, y=282} False NoOp
-        [ Control Numeric "Players" 'P' {x=8, y=40}
-          {min=2, max=10, step=1, getter=(\config -> config.playerCount ), setter=(\config n -> { config | playerCount = n })}
-        --}
+        , Control "Players" 'P' {x=8, y=40}
+          (Numeric (NumericSpec 2 10 1 None (\config -> config.playerCount) (\config n -> { config | playerCount = n })))
 
         , Control "Rounds" 'R' {x=8, y=70}
           (Numeric (NumericSpec 5 1000 5 None (\config -> config.roundCount) (\config n -> { config | roundCount = n })))
