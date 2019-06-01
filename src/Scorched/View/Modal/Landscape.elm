af5af0a986d62a75c03c73b882d8e5d9592115b8
Sonny Michaud <michaud.sonny@gmail.com>
2019-05-31 23:10:52 -0500

    add control to landscape modal

--- src/Scorched/View/Modal/Landscape.elm
+++ src/Scorched/View/Modal/Landscape.elm
@@ -1,14 +1,25 @@
 module Scorched.View.Modal.Landscape exposing (build)
 
+import Dict exposing (Dict)
+
 import Svg exposing (Svg)
 import Svg.Attributes as Attr
 
-import Scorched.Model.Types exposing (Model, Msg)
+import Scorched.Model.Types exposing (..)
 
 import Scorched.View.Component.BorderBox as BorderBox
+import Scorched.View.Component.Control as Control
 
 build : Model -> Svg Msg
-build model =
+build {config} =
   Svg.g
     [ Attr.transform "translate(410, 345)" ]
-    [ BorderBox.build {width=300, height=240} 2 False ]
+    [ BorderBox.build {width=300, height=240} 2 False
+    , Svg.g
+      [ Attr.id "landscape-controls" ]
+      (List.map (Control.build config False) controls)
+    ]
+
+controls : List Control
+controls =
+  [ Control "Bumpiness" 'B' {x=7, y=12} (Button (ButtonSpec {width=82, height=19} False NoOp)) ]
