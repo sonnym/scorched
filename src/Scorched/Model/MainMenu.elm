cc59fb55e445b938726d6e9f6cc053aaa28d36aa
Sonny Michaud <michaud.sonny@gmail.com>
2019-05-31 22:57:10 -0500

    re-order list elements to reflect actual position

--- src/Scorched/Model/MainMenu.elm
+++ src/Scorched/Model/MainMenu.elm
@@ -22,6 +22,13 @@ defaultControls =
     (List.map
       (\control -> (control.label, control))
         [ Control "Start" 'S' {x=7, y=12} (Button (ButtonSpec {width=82, height=19} False NoOp))
+
+        , Control "Players" 'P' {x=8, y=40}
+          (Numeric (NumericSpec 2 10 1 None (\config -> config.playerCount) (\config n -> { config | playerCount = n })))
+
+        , Control "Rounds" 'R' {x=8, y=70}
+          (Numeric (NumericSpec 5 1000 5 None (\config -> config.roundCount) (\config n -> { config | roundCount = n })))
+
         , Control "Sound…" 'o' {x=7, y=102} (Button (ButtonSpec {width=56, height=19} False NoOp))
         , Control "Hardware…" 'H' {x=7, y=132} (Button (ButtonSpec {width=78, height=19} False NoOp))
         , Control "Economics…" 'E' {x=7, y=162} (Button (ButtonSpec {width=84, height=19} False NoOp))
@@ -29,12 +36,6 @@ defaultControls =
         , Control "Landscape…" 'L' {x=7, y=222} (Button (ButtonSpec {width=82, height=19} False (UpdateView (Modal Landscape))))
         , Control "Play Options…" 't' {x=7, y=252} (Button (ButtonSpec {width=98, height=19} False NoOp))
         , Control "Weapons…" 'W' {x=7, y=282} (Button (ButtonSpec {width=68, height=19} False NoOp))
-
-        , Control "Players" 'P' {x=8, y=40}
-          (Numeric (NumericSpec 2 10 1 None (\config -> config.playerCount) (\config n -> { config | playerCount = n })))
-
-        , Control "Rounds" 'R' {x=8, y=70}
-          (Numeric (NumericSpec 5 1000 5 None (\config -> config.roundCount) (\config n -> { config | roundCount = n })))
         ]
     )
 
