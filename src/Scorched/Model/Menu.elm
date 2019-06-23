3cc78d1016b9fc0e38db9e933d8a71a175a875f9
Sonny Michaud <michaud.sonny@gmail.com>
2019-06-23 15:19:01 -0500

    restore keyboard message handling on main menu

--- src/Scorched/Model/Menu.elm
+++ src/Scorched/Model/Menu.elm
@@ -1,27 +1,25 @@
 module Scorched.Model.Menu exposing (handleKeyDown, handleKeyUp, handleKeyPress)
 
-import Scorched.Model.Types exposing (Model, Msg, Menu_(..))
+import Scorched.Model.Types exposing (Model, Msg, Menu_(..), Configuration, MainMenuData)
 
+import Scorched.Model.Control as Control
 import Scorched.Model.Menu.Main as MainMenu
 
 handleKeyDown : Menu_ -> Model -> String -> (Model, Cmd Msg)
-handleKeyDown menu model key = (model, Cmd.none)
-      -- ({ model | menuData = toggleControlByKey model.menuData key }, Cmd.none)
+handleKeyDown menu model key =
+  case menu of
+    Main -> ({ model | menuData = toggleControlByKey model.menuData key }, Cmd.none)
 
 handleKeyUp : Menu_ -> Model -> String -> (Model, Cmd Msg)
-handleKeyUp menu model key = (model, Cmd.none)
-      -- ({ model | menuData = toggleControlByKey model.menuData key }, Cmd.none)
+handleKeyUp menu model key =
+  case menu of
+    Main -> ({ model | menuData = toggleControlByKey model.menuData key }, Cmd.none)
 
 handleKeyPress : Menu_ -> Model -> String -> (Model, Cmd Msg)
-handleKeyPress menu model key = (model, Cmd.none)
-      -- (model, handleKeyPress model.config key)
-
-{--
-handleKeyPress : Configuration -> String -> Cmd Msg
-handleKeyPress configuration label =
-  (Control.handleKeyPress defaultControls) configuration label
+handleKeyPress menu ({config} as model) key =
+  case menu of
+    Main -> (model, (Control.handleKeyPress MainMenu.default.controls) config key)
 
 toggleControlByKey : MainMenuData -> String -> MainMenuData
 toggleControlByKey menuData key =
-  (Control.toggleControlByKey defaultControls) menuData key
---}
+  (Control.toggleControlByKey MainMenu.default.controls) menuData key
