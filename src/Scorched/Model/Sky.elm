15a96ffa4996612a3d88504ea61dca31564c4648
Sonny Michaud <michaud.sonny@gmail.com>
2019-06-26 01:45:37 -0500

    prevent random empty sky

--- src/Scorched/Model/Sky.elm
+++ src/Scorched/Model/Sky.elm
@@ -11,13 +11,16 @@ default : Sky
 default = Random
 
 all : List Sky
-all = [ Random, PitchBlack, Plain, Sunset ]
+all = Random :: exceptRandom
 
 generator : Config -> Random.Generator Sky
 generator config =
   case config.worldConfig.sky of
-    Random -> Random.uniform empty all
+    Random -> Random.uniform empty exceptRandom
     _ -> Random.constant config.worldConfig.sky
+
+exceptRandom : List Sky
+exceptRandom = [ PitchBlack, Plain, Sunset ]
 
 toString : Sky -> String
 toString sky =
