6567479fe3267bbcee665c9f448e2135ab4097d2
Sonny Michaud <michaud.sonny@gmail.com>
2019-07-01 21:04:44 -0500

    draw top bar

--- src/Scorched/View/GamePlay.elm
+++ src/Scorched/View/GamePlay.elm
@@ -1,8 +1,18 @@
 module Scorched.View.GamePlay exposing (build)
 
 import Svg exposing (Svg)
+import Svg.Attributes as Attr
 
 import Scorched.Model.Types exposing (Model, Msg)
 
+import Scorched.View.Component.BorderBox as BorderBox
+
 build : Model -> Svg Msg
-build model = Svg.g [] []
+build model =
+  Svg.g
+    [ Attr.id "game-play" ]
+    [ topBar model ]
+
+topBar : Model -> Svg Msg
+topBar {dimensions} =
+  BorderBox.build {width=dimensions.width, height=21} 2 False
