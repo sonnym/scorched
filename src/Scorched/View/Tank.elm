e940911e25eba145f76bd8909ea11b40d9f69a8a
Sonny Michaud <michaud.sonny@gmail.com>
2019-07-02 00:06:38 -0500

    position tanks from bottom of world

--- src/Scorched/View/Tank.elm
+++ src/Scorched/View/Tank.elm
@@ -3,12 +3,12 @@ module Scorched.View.Tank exposing (build)
 import Svg exposing (Svg)
 import Svg.Attributes as Attr
 
-import Scorched.Model.Types exposing (Player, Msg)
+import Scorched.Model.Types exposing (Player, Msg, Dimension)
 
-build : Player -> Svg msg
-build {position} =
+build : Dimension -> Player -> Svg msg
+build {height} {position} =
   Svg.circle
     [ Attr.cx (String.fromInt position.x)
-    , Attr.cy (String.fromInt position.y)
+    , Attr.cy (String.fromInt (height - position.y))
     , Attr.r "10"
     ] []
