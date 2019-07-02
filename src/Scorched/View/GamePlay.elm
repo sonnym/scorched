43896309ef695d980a48ae9190a2020e5d021293
Sonny Michaud <michaud.sonny@gmail.com>
2019-07-01 21:19:12 -0500

    draw world

--- src/Scorched/View/GamePlay.elm
+++ src/Scorched/View/GamePlay.elm
@@ -5,14 +5,26 @@ import Svg.Attributes as Attr
 
 import Scorched.Model.Types exposing (Model, Msg)
 
+import Scorched.View.World as World
 import Scorched.View.Component.BorderBox as BorderBox
 
 build : Model -> Svg Msg
 build model =
   Svg.g
     [ Attr.id "game-play" ]
-    [ topBar model ]
+    [ topBar model, world model ]
 
 topBar : Model -> Svg Msg
 topBar {dimensions} =
-  BorderBox.build {width=dimensions.width, height=21} 2 False
+  Svg.g
+    [ Attr.id "world"
+    , Attr.transform "translate(2, 2)"
+    ]
+    [ BorderBox.build {width=dimensions.width, height=21} 2 False ]
+
+world : Model -> Svg Msg
+world model =
+  Svg.g
+    [ Attr.id "world"
+    , Attr.transform "translate(3, 22)"
+    ] [ World.build model.world]
